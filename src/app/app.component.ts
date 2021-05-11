7c5898abc52e066246f5d232e8a2a9df53fcc1fc
PatrickSys <rossellocolompatrick@gmail.com>
2021-05-11 18:11:19 +0200

    routing enabled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,8 +4,10 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { Employee } from './employees/employee';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
+
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
@@ -14,9 +16,11 @@ export class AppComponent implements OnInit{
   public employees: Employee[];
 
 
-  constructor(private employeeService: EmployeeService){}
+  constructor(private employeeService: EmployeeService, private router: Router){}
 
   ngOnInit() {
+
+    this.router.navigate(['home'])
     this.employeeService.getEmployees();
   }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
-
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { PayrollComponent } from './payroll/payroll.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MainpageComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    PayrollComponent,
+
     ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     AutocompleteLibModule,
-    FormsModule
+    FormsModule,
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
